75b7e7ba86f71aad530ede1e8a0db03a3b98ed6e
Jacob Levinson <jacoblevinson111@gmail.com>
2024-11-22 14:20:45 -0800

    Working start stop

--- software/start_wifi.sh
+++ software/start_wifi.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-echo "Re-enabling Wi-Fi services..."
-
-# Re-enable and start wpa_supplicant
-sudo systemctl enable wpa_supplicant
-sudo systemctl start wpa_supplicant
-
-# Bring wlan0 interface up and request an IP address
-sudo ifconfig wlan0 up
-sudo dhclient wlan0
-
-echo "Wi-Fi reconnected."
+echo "Enabling wlan0 interface..."
+sudo ifconfig wlan0 up  # Directly bring up the interface
--- software/stop_wifi.sh
+++ software/stop_wifi.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-echo "Stopping wpa_supplicant service..."
-sudo systemctl stop wpa_supplicant
-sudo systemctl disable wpa_supplicant
-
 echo "Disabling wlan0 interface..."
-sudo ifconfig wlan0 down  # Use ifconfig as it works reliably on your setup
-sudo ip addr flush dev wlan0
-
-echo "Wi-Fi fully disabled. Ready for Wi-Fi Direct."
+sudo ifconfig wlan0 down  # Directly bring up the interface
