81dac35627f8d8185970260237b44fe898e26e9e
James Shiffer <jshiffer@ucla.edu>
2024-12-05 21:02:04 -0800

    Fix connection callback problem

--- software/scanner.py
+++ software/scanner.py
@@ -28,56 +28,42 @@ class BLEServiceScanner:
 
     async def connection_callback(self, client):
         """Callback for when a device is paired"""
-        # Get all services
-        services = await client.get_services()
+        self.logger.info(f"Reading characteristics for service")
         
-        # Find our target service
-        target_service = None
-        for service in services:
-            if str(service.uuid).lower() == self.target_service_uuid.lower():
-                target_service = service
+        try:
+            # first, send our metadata (server may want one of our chunks)
+            our_data = {
+                "ssid": self.ssid,
+                "manifest": self.manifest,
+            }
+            our_data_str = json.dumps(our_data)
+            await client.write_gatt_char(PKG_MANIFEST_W, our_data_str.encode('utf-8'))
+            self.logger.info(f"Sent our package manifest")
+
+            # read their manifest
+            pkg_list_raw = await client.read_gatt_char(PKG_LIST_R)
+            pkg_list = json.loads(pkg_list_raw)
+            self.logger.info(f"Got package manifest: {pkg_list_raw}")
+
+            self.peers[pkg_list["mac"]] = pkg_list["pkgs"]
+            for pkg_name in pkg_list["pkgs"]:
+                await client.write_gatt_char(PKG_REQUEST_W, pkg_name.encode('utf-8'))
+                pkg_manifest_raw = await client.read_gatt_char(PKG_MANIFEST_R)
+                self.logger.info(f"Got package manifest: {pkg_manifest_raw}")
+                pkg_manifest = json.loads(pkg_manifest_raw)
+
+                if pkg_name not in self.packages:
+                    self.packages[pkg_name] = Package(name, 1)
+
+                # include peer ssid in response, so we can connect to wifi
+                response = {"ssid": pkg_list["ssid"], "manifest": pkg_manifest}
+                self.on_manifest(response)
+
+                # TODO: support multiple pkgs, for now break after the first one
                 break
-        
-        if target_service:
-            self.logger.info(f"Reading characteristics for service: {target_service.uuid}")
-            
-            try:
-                # first, send our metadata (server may want one of our chunks)
-                our_data = {
-                    "ssid": self.ssid,
-                    "manifest": self.manifest,
-                }
-                our_data_str = json.dumps(our_data)
-                await client.write_gatt_char(PKG_MANIFEST_W, our_data_str.encode('utf-8'))
-                self.logger.info(f"Sent our package manifest")
 
-                # read their manifest
-                pkg_list_raw = await client.read_gatt_char(PKG_LIST_R)
-                pkg_list = json.loads(pkg_list_raw)
-                self.logger.info(f"Got package manifest: {pkg_list_raw}")
-
-                self.peers[pkg_list["mac"]] = pkg_list["pkgs"]
-                for pkg_name in pkg_list["pkgs"]:
-                    await client.write_gatt_char(PKG_REQUEST_W, pkg_name.encode('utf-8'))
-                    pkg_manifest_raw = await client.read_gatt_char(PKG_MANIFEST_R)
-                    self.logger.info(f"Got package manifest: {pkg_manifest_raw}")
-                    pkg_manifest = json.loads(pkg_manifest_raw)
-
-                    if pkg_name not in self.packages:
-                        self.packages[pkg_name] = Package(name, 1)
-
-                    # include peer ssid in response, so we can connect to wifi
-                    response = {"ssid": pkg_list["ssid"], "manifest": pkg_manifest}
-                    self.on_manifest(response)
-
-                    # TODO: support multiple pkgs, for now break after the first one
-                    break
-
-            except Exception as e:
-                print("Failed to package list from characteristics", str(e))
-
-        else:
-            self.logger.warning("Target service not found on device")
+        except Exception as e:
+            print("Failed to package list from characteristics", str(e))
 
     async def scan_and_read(self, manifest: dict, scan_duration=5):
         """Scan for devices and read characteristics of matching ones"""
