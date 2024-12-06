a14e49ace138889af5c40693f1621784ec3e5b51
Jacob Levinson <jacoblevinson111@gmail.com>
2024-12-05 20:03:45 -0800

    change uuid characteristics

--- software/config.py
+++ software/config.py
@@ -2,10 +2,10 @@
 UUID = "de960fd7-befb-4dab-aa6c-29449c725039"
 
 # Bluetooth characteristic IDs
-PKG_LIST_R = "BEF0"
-PKG_REQUEST_W = "BEF1"
-PKG_MANIFEST_R = "BEF2"
-PKG_MANIFEST_W = "BEF3"
+PKG_LIST_R = "de960fd7-0001-4dab-aa6c-29449c725039"
+PKG_REQUEST_W = "de960fd7-0002-4dab-aa6c-29449c725039"
+PKG_MANIFEST_R = "de960fd7-0003-4dab-aa6c-29449c725039"
+PKG_MANIFEST_W = "de960fd7-0004-4dab-aa6c-29449c725039"
 
 import os
 FILE_DIR = os.path.abspath(os.path.dirname(__file__) + '/downloads')  # Directory for files
