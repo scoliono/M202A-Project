b517465489dc763b715b05923c284e270094c0d6
James Shiffer <jshiffer@ucla.edu>
2024-12-05 20:52:06 -0800

    fix callback bug, change scan duration to constant

--- software/main.py
+++ software/main.py
@@ -71,8 +71,7 @@ async def main():
             state = State.BT_ADVERT
             await ble_server(packages, on_manifest_received)
             state = State.BT_SCAN
-            duration = random.randint(10, 20)
-            await scanner.scan_and_read(our_manifest, scan_duration=duration)
+            await scanner.scan_and_read(our_manifest, scan_duration=3)
 
         # is there is no difference between manifests?
         if not pkg.manifests_differ(our_manifest, peer_manifest):
